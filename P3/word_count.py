527c5e34fa6be7203bcd9f75fe2ea1bbcad5e4c4
A00967999 - Gerardo Quiroga <113482664+gquiro86@users.noreply.github.com>
2024-02-01 19:21:05 -0600

    commit

--- P3/word_count.py
+++ P3/word_count.py
@@ -1,11 +1,15 @@
+"""Module providing a method to count words from a given file in .txt."""
+
 import sys
 import time
 
-# Función para leer el archivo y contar las palabras
 def contar_palabras(nombre_archivo):
+    """
+    Cuenta las palabras en un archivo y devuelve un diccionario con las frecuencias.
+    """
     conteo_palabras = {}
     try:
-        with open(nombre_archivo, 'r') as archivo:
+        with open(nombre_archivo, 'r', encoding='utf-8') as archivo:
             for linea in archivo:
                 palabras = linea.split()
                 for palabra in palabras:
@@ -17,15 +21,14 @@ def contar_palabras(nombre_archivo):
                             conteo_palabras[palabra] = 1
     except FileNotFoundError:
         print("Error: Archivo no encontrado.")
-    except Exception as e:
-        print("Se produjo un error al procesar el archivo:", e)
-
     return conteo_palabras
 
-# Función para imprimir y escribir los resultados en un archivo
 def imprimir_y_escribir_resultados(conteo_palabras, tiempo_transcurrido):
+    """
+    Se adiciona el texto en el archivo de salida.
+    """
     palabras_ordenadas = sorted(conteo_palabras.items(), key=lambda x: x[0])
-    with open('resultados_conteo_palabras.txt', 'w') as archivo_resultado:
+    with open('WordCountResults.txt', 'w', encoding='utf-8') as archivo_resultado:
         archivo_resultado.write("Palabra\tFrecuencia\n")
         for palabra, frecuencia in palabras_ordenadas:
             archivo_resultado.write(f"{palabra}\t{frecuencia}\n")
@@ -37,12 +40,15 @@ def imprimir_y_escribir_resultados(conteo_palabras, tiempo_transcurrido):
     print(f"\nTiempo transcurrido: {tiempo_transcurrido:.6f} segundos")
 
 if __name__ == "__main__":
+    """
+    MAIN
+    """
     if len(sys.argv) != 2:
-        print("Uso: python word_count .py archivos_con_datos.txt")
+        print("Uso: python wordCount.py archivoConDatos.txt")
     else:
         tiempo_inicio = time.time()
-        nombre_archivo = sys.argv[1]
-        conteo_palabras = contar_palabras(nombre_archivo)
+        nombre_archivo = sys.argv[1]  # pylint: disable=W0621
+        conteo_palabras = contar_palabras(nombre_archivo)  # pylint: disable=W0621
         tiempo_fin = time.time()
         tiempo_transcurrido = tiempo_fin - tiempo_inicio
 
