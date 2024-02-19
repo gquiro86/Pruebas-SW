0542806c1c409013f2320b866791e86bb90b18a0
A00967999 - Gerardo Quiroga <113482664+gquiro86@users.noreply.github.com>
2024-02-18 19:31:41 -0600

    main

--- A00967999_A6.2/test/unit_test_hotel.py
+++ A00967999_A6.2/test/unit_test_hotel.py
@@ -1,22 +1,22 @@
 import unittest
-from app_hotel import Hotel
+from app.hotel import Hotel 
 import HtmlTestRunner
 
 class TestHotelMethods(unittest.TestCase):
     def test_display_info(self):
-        hotel = Hotel("Hotel Fiesta Inn", "Ciudad de Mexico", 100)
-        self.assertEqual(hotel.display_info(), "Hotel: Hotel Fiesta Inn, Location: Ciudad de Mexico, Rooms: 100")
+        hotel = Hotel("Hotel California", "California", 100)
+        self.assertEqual(hotel.display_info(), "Hotel: Hotel California, Location: California, Rooms: 100")
 
     def test_reserve_room(self):
-        hotel = Hotel("Hotel Fiesta Inn", "Ciudad de Mexico", 100)
-        hotel.reserve_room("Gerardo Quiroga")
-        self.assertIn("Gerardo Quiroga", [customer.name for customer in hotel.reservations])
+        hotel = Hotel("Hotel California", "California", 100)
+        hotel.reserve_room("John Doe")
+        self.assertIn("John Doe", [customer.name for customer in hotel.reservations])
 
     def test_cancel_reservation(self):
-        hotel = Hotel("Hotel Fiesta Inn", "Ciudad de Mexico", 100)
-        hotel.reserve_room("Gerardo Quiroga")
-        hotel.cancel_reservation("Gerardo Quiroga")
-        self.assertNotIn("Gerardo Quiroga", [customer.name for customer in hotel.reservations])
+        hotel = Hotel("Hotel California", "California", 100)
+        hotel.reserve_room("John Doe")
+        hotel.cancel_reservation("John Doe")
+        self.assertNotIn("John Doe", [customer.name for customer in hotel.reservations])
 
 if __name__ == '__main__':
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='hotel_reports'))
